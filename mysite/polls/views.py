60a3f1947f6f4b71674c1cc5c9d0fa8ea77ca3a9
shivam <shvmjndl2@gmail.com>
2016-03-27 03:22:55 +0530

    Initial commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Question
+    # ...
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+            raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    
